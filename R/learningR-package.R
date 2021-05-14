e41bf7cd7cb1597c263c53d9254fa2713b22cc6c
Andreas Blaette <andreas.blaette@uni-due.de>
2021-05-14 13:58:05 +0200

    R package structure

---
+++ R/learningR-package.R
@@ -0,0 +1,9 @@
+#' learningR package.
+#' 
+#' @author Andreas Blaette (andreas.blaette@@uni-due.de)
+#' @keywords package
+#' @docType package
+#' @rdname learningR-package
+#' @name learningR
+NULL
+
--- tasks/hausaufgabe.R
+++
@@ -1,8 +0,0 @@
-countries <- levels(ess6$cntry)
-for (country in countries){
-  
-}
-
-plot(..., type="n") # leeres Diagramm
-plot() # Punkte
-text() # Beschriftung Punkte
