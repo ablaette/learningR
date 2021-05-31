1d153fd60d1eefc6bbd5877b49bee991f2a0f6b7
Andreas Blaette <andreas.blaette@uni-due.de>
2021-05-31 12:50:29 +0200

    add pkg to drat

--- R/learningR-package.R
+++ R/learningR-package.R
@@ -18,8 +18,32 @@ NULL
 
 #' Corona auf Ebene der Landkreise.
 #'
+#'
 #' @docType data
 #' @keywords datasets
+#' @format A `data.frame` with 400 rows (representing counties) and 13 columns (variables):
+#' \describe{
+#'   \item{RS}{Abbreviation for "Regionalschluessel", a unique key issued for
+#'   each county. One to two digits for the regional state, one for the
+#'   administrative district (Regierungsbezirk) and two digits for the county
+#'   ("Landkreis") or "kreisfreie Stadt".
+#'   }
+#'   \item{name}{Name of the "Landkreis" or "kreisfreie Stadt".}
+#'   \item{region}{Regional stated (abbreviated, two uppercase letters),}
+#'   \item{type}{Type of regional entity (Landkreis / kreisfreie Stadt).}
+#'   \item{population_total}{Total population living in the county. Source: ...}
+#'   \item{cases7_per_100k}{7-Tage-Inzidenz, Stand: ...}
+#'   \item{death_rate}{Todesrate, }
+#'   \item{foreign_pop_total}{Total number of foreigners / persons without
+#'   German citizenship. Data source: Migration.Integration.Regionen, Gemeinsames Datenangebot von Destatis, BA und BAMF,
+#'   data for
+#'   \href{https://service.destatis.de/DE/karten/migration_integration_regionen.html#ANT_AI}{map on share of foreigenrs in regions (31.12.2019)}
+#'   }
+#'   \item{afd}{Percantage of the share of AfD }
+#'   \item{income}{Average household income. Source: \href{http://rstudio.com}{Rstudio}}
+#'   \item{per_100km2}{Population density.}
+#'   \item{agequot}{Share of persons aged 65 or older.}
+#' }
 "corona_by_county"
 
 #' Open R Markdown sourcecode in RStudio
--- data-raw/corona_lkr.R
+++ data-raw/corona_lkr.R
@@ -4,7 +4,7 @@ library(openxlsx)
 
 mi_url <- "https://service.destatis.de/DE/karten/data/migration_integration_regionen.zip"
 mi_reg <- file.path(tempdir(), basename(mi_url))
-download.file(url = url, destfile = mi_reg)
+download.file(url = mi_url, destfile = mi_reg)
 setwd(tempdir())
 unzip(zipfile = mi_reg)
 
@@ -21,6 +21,32 @@ rki <- read.csv(file = "~/Lab/github/learningR/data-raw/RKI_Corona_Landkreise.cs
   select(c("RS", "BEZ", "death_rate", "cases7_per_100k"))
 
 
+## Altersquotient
+
+age <- read.csv(file = "~/Lab/github/learningR/data-raw/12411-0017.csv", skip = 5, sep = ";", fileEncoding = "latin1")
+cols <- unname(unlist(read.csv(file = "~/Lab/github/learningR/data-raw/12411-0017.csv", skip = 5, nrows = 1, header = FALSE, sep = ";")))
+cols[1:3] <- c("date", "RS", "region")
+colnames(age) <- cols
+age <- filter(age, !is.na(RS))
+agequot <- data.frame(
+  RS = age$RS,
+  agequot = round((as.integer(age[["65 bis unter 75 Jahre"]]) + as.integer(age[["75 Jahre und mehr"]])) / as.integer(age[["Insgesamt"]]) * 100, 1)
+)
+
+
+## Bevölkerungsdichte
+
+url_density <- "https://www.destatis.de/DE/Themen/Laender-Regionen/Regionales/Gemeindeverzeichnis/Administrativ/04-kreise.xlsx;jsessionid=2CBF45B087AEF3907975E8C64A6A0997.live711?__blob=publicationFile"
+xlsx_local_density <- file.path(tempdir(), strsplit(basename(url_density), ";")[[1]][1])
+download.file(url_density, destfile = xlsx_local_density)
+
+density <- openxlsx::read.xlsx(xlsx_local_density, sheet = 2, startRow = 6)
+colnames(density) <- c("RS", "RegBez", "Region", "NUTS3", "km2", "BevInsg", "BevM", "BevW", "per_km2")
+density_min <- filter(density, !is.na(NUTS3)) %>%
+  select(RS, per_km2)
+density_min$RS <- as.integer(density_min$RS)
+
+
 ## Einkommen der Haushalte
 # Quelle: https://www.statistikportal.de/de/vgrdl/ergebnisse-kreisebene/einkommen-kreise
 
@@ -28,7 +54,7 @@ url <- "https://www.statistikportal.de/sites/default/files/2021-05/vgrdl_r2b3_bs
 xlsx_local <- file.path(tempdir(), basename(url))
 download.file(url, destfile = xlsx_local)
 
-raw <- openxlsx::read.xlsx(xlsx_local, sheet = "1.4")
+raw <- openxlsx::read.xlsx(xlsx_local, sheet = "2.4")
 df <- raw[5:nrow(raw),]
 colnames(df) <- raw[4,]
 df[["RS"]] <- as.integer(df[["Regional-schlüssel"]])
@@ -42,9 +68,16 @@ econ <- as_tibble(df) %>%
 corona_by_county <- inner_join(x = mi, y = afd, by = "RS") %>%
   inner_join(y = rki, by = "RS") %>%
   left_join(y = econ, by = "RS") %>%
+  left_join(y = density_min, by = "RS") %>%
+  left_join(y = agequot, by = "RS") %>%
   select(!Name) %>%
   rename(name = "NAME",  population_total = "INSGESAMT", foreign_pop_total = "AUSL_INSG", foreign_pop_share = "ANT_AI", afd = "AfD", type = "BEZ", region = "Land") %>%
   relocate(RS, name, region, type, population_total, cases7_per_100k, death_rate, foreign_pop_total, foreign_pop_share, afd)
 
 
 save(corona_by_county, file = "~/Lab/github/learningR/data/countydata.RData")
+
+# w <- openxlsx::createWorkbook()
+# openxlsx::addWorksheet(wb = w, sheetName = "coronastats")
+# openxlsx::writeData(wb = w, sheet = "coronastats", x = corona_by_county)
+# openxlsx::saveWorkbook(wb = w, file = "~/Lab/tmp/coronastats.xlsx")
